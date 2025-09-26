6217ee7e161bf237cbdf01a2d64ccdea98451e03
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-09-26 19:13:07 +0530

    adding

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "sree84s.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default = "Z0897598ZW3D3944XUN0"
+  description = "description"
+}
